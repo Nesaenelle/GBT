707466e7a8b45b16dbde8716202d74ea8d0e6cea
Bogdan Zinkevych <venomsunset@gmail.com>
2019-04-17 16:52:06 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -7,30 +7,49 @@ $(document).ready(function () {
         infinite: true,
         speed: 300,
         slidesToShow: 6,
-
-        // prevArrow: '.slick-prev',
-        // nextArrow: '.slick-next',
-        responsive: [{
-            breakpoint: 1280,
-            settings: {
-                centerMode: true,
-                slidesToShow: 2
+        responsive: [
+            {
+                breakpoint: 1600,
+                settings: {
+                    // centerMode: true,
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 1280,
+                settings: {
+                    // centerMode: true,
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    // centerMode: true,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    // centerMode: true,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    // arrows: false,
+                    // dots: false,
+                    // centerMode: true,
+                    // centerPadding: '20px',
+                    slidesToShow: 1
+                }
             }
-        },
-        {
-            breakpoint: 720,
-            settings: {
-                arrows: false,
-                dots: false,
-                centerMode: true,
-                centerPadding: '20px',
-                slidesToShow: 1
-            }
-        }
         ]
     });
 
-    $('#adolescence-form').on('submit', (e)=>{
+    $('#adolescence-form').on('submit', (e) => {
         e.preventDefault();
         console.log($(e.currentTarget).serialize());
     });
