24186585c70315ebddb8e461bd94d1af06ec81fb
Bogdan Zinkevych <venomsunset@gmail.com>
2019-04-19 21:41:15 +0300

    fix

--- src/animate.js
+++ src/animate.js
@@ -1,32 +1,38 @@
-var elements = document.querySelectorAll('[data-animate]');
-
-window.addEventListener('scroll', function () {
-    elements.forEach(function (elem) {
-        if (isInViewport(elem, 150)) {
-            if (!elem.getAttribute('data-animate')) {
-                elem.setAttribute('data-animate', true);
+; (function () {
+    var elements = document.querySelectorAll('[data-animate]');
+    function update() {
+        elements.forEach(function (elem) {
+            if (isInViewport(elem, 150)) {
+                if (!elem.getAttribute('data-animate')) {
+                    elem.setAttribute('data-animate', true);
+                }
             }
-        }
-    });
-});
-
-
-function isInViewport(el, offset) {
-    var top = el.offsetTop + offset;
-    var left = el.offsetLeft;
-    var width = el.offsetWidth;
-    var height = el.offsetHeight;
-
-    while (el.offsetParent) {
-        el = el.offsetParent;
-        top += el.offsetTop;
-        left += el.offsetLeft;
+        });
     }
 
-    return (
-        top < (window.pageYOffset + window.innerHeight) &&
-        left < (window.pageXOffset + window.innerWidth) &&
-        (top + height) > window.pageYOffset &&
-        (left + width) > window.pageXOffset
-    );
-};
+    update();
+    window.addEventListener('scroll', function () {
+        update();
+    });
+
+
+    function isInViewport(el, offset) {
+        var top = el.offsetTop + offset;
+        var left = el.offsetLeft;
+        var width = el.offsetWidth;
+        var height = el.offsetHeight;
+
+        while (el.offsetParent) {
+            el = el.offsetParent;
+            top += el.offsetTop;
+            left += el.offsetLeft;
+        }
+
+        return (
+            top < (window.pageYOffset + window.innerHeight) &&
+            left < (window.pageXOffset + window.innerWidth) &&
+            (top + height) > window.pageYOffset &&
+            (left + width) > window.pageXOffset
+        );
+    };
+})();
